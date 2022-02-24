420e634c530f318e90b7e534d2ef56b6d0c047a7
Samuel Colvin <s@muelcolvin.com>
2022-02-24 20:49:44 +0000

    adding ansi output to docs

--- docs/plugins.py
+++ docs/plugins.py
@@ -1,4 +1,9 @@
 import logging
+from pathlib import Path
+
+from pygments.styles.xcode import XcodeStyle
+from pygments_ansi_color import color_tokens
+from pymdownx.highlight import HtmlFormatter
 
 try:
     import pytest
@@ -8,7 +13,12 @@ except ImportError:
 logger = logging.getLogger('mkdocs.test_examples')
 
 
-def test_examples(config):
+def on_pre_build(config):
+    test_examples()
+    build_ansi_styles()
+
+
+def test_examples():
     """
     Plug called by mkdocs-simple-hooks to run the examples tests.
     """
@@ -19,3 +29,29 @@ def test_examples(config):
         return_code = pytest.main(['-q', '-p', 'no:sugar', 'tests/test_docs.py'])
         if return_code != 0:
             logger.warning('examples tests failed')
+
+
+def build_ansi_styles():
+    styles_file = Path(__file__).parent / 'css/ansi.css'
+    if styles_file.exists():
+        return
+
+    colours = {
+        'Black': '#000000',
+        'Red': '#EF2929',
+        'Green': '#8AE234',
+        'Yellow': '#FCE94F',
+        'Blue': '#3465A4',
+        'Magenta': '#c509c5',
+        'Cyan': '#34E2E2',
+        'White': '#ffffff',
+    }
+
+    class MyStyle(XcodeStyle):
+        styles = dict(XcodeStyle.styles)
+        styles.update(color_tokens(colours, colours))
+
+    formatter = HtmlFormatter(style=MyStyle)
+    styles_file.parent.mkdir(exist_ok=True)
+    styles_file.write_text(formatter.get_style_defs())
+    logger.info('generated ansi styles file=%s', styles_file)
--- docs/usage.md
+++ docs/usage.md
@@ -1,8 +1,23 @@
-To do:
+## Boolean Logic
 
-* initialised comparison vs. bare class comparison
-* accessing `.value`
-* boolean logic
-* Custom classes
-* mypy and types
+## Initialised vs. Class comparison
+
+*dirty-equals* allows comparison with types regardless of whether they've been initialised.
+
+This saves users adding `()` in lots of places.
+
+Example:
+
+```py title="Initialised vs. Uninitialised"
+from dirty_equals import IsInt
+
+# these two cases are the same
+assert 1 == IsInt
+assert 1 == IsInt()
+```
+
 * pytest compatibility, including `__eq__` vs `__ne__`
+
+```ansi-color
+{! docs/test.ansi !}
+```
